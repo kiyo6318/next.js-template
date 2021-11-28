7bdecb09c64bfa185c8394e7d85d0ebe22e3be70
kiyo <kiyonari6318@yahoo.co.jp>
2021-11-28 12:36:09 +0900

    Fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import type { AppProps } from 'next/app';
+import { AppProps } from 'next/app';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
